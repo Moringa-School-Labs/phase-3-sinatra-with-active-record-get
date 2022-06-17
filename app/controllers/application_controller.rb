ddd4d239b472712a0f1ef53f4e0290f2359689e9
catherine <vuthi.esther@gmail.com>
2022-06-17 22:36:23 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  # Returns all games in db
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  # Returns specific game by id
+  get '/games/:id' do
+   game = Game.find(params[:id])
+
+   # Add include method to include data about associated models in our json 
+   game.to_json(include: :reviews)
+   game.to_json(include: {reviews: {include: :user }})
   end
 
 end
